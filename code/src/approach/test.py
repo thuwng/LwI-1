93f8baa030756a5bd422d77f4042fda5e39de910
chenzk202212 <158129746+chenzk202212@users.noreply.github.com>
2024-05-21 17:05:49 +0800

    Update test.py

--- code/src/approach/test.py
+++ code/src/approach/test.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 from src.datasets.exemplars_dataset import ExemplarsDataset
 from src.approach.incremental_learning import Inc_Learning_Appr
 
-
+  
 class Appr(Inc_Learning_Appr):
     def __init__(self, model, device, nepochs=100, lr=0.05, lr_min=1e-4, lr_factor=3, lr_patience=5, clipgrad=10000,
                  momentum=0, wd=0, multi_softmax=False, wu_nepochs=0, wu_lr_factor=1, fix_bn=False, eval_on_train=False,
